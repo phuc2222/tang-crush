6634675e1a05e1500d8a61a2bd4b0340e91ce26c
phuc2222 <85415254+phuc2222@users.noreply.github.com>
2021-06-06 03:21:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Công iu dấu à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -11,9 +11,9 @@ const CONFIG = {
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    reply: 'Địt mẹ công óc chó vcl <33333333',
+    mess: 'Anh biết mà 🥰. Yêu công nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón nhé công bê đê.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://m.me/100022968820093' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
